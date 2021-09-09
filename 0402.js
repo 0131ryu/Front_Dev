23357f240b9ef7b34fffea6d839d97b204182533
0131ryu <89256977+0131ryu@users.noreply.github.com>
2021-09-09 23:36:12 +0900

    0909 Javascript 다시 공부
    참고 : 모던 자바스크립트 Deep Dive (01 ~ 05)

---
+++ 0402.js
@@ -0,0 +1,14 @@
+//변수 : 하나의 값을 저장하기 위한 메커니즘
+
+var userId = 1;
+var userName = 'Lee';
+
+//그룹화 가능
+var user = { id: 1, name: 'Lee'};
+
+var users = [
+    { id : 1, name : 'Lee'},
+    { id : 2, name : 'Kim' }
+];
+
+var result = 10 + 20;
---
+++ 0408.js
@@ -0,0 +1,16 @@
+console.log(score);
+console.log(time);
+console.log(music);
+
+var score;
+score = 80;
+
+var time = 100;
+time = 90;
+
+music = 70;
+var music;
+
+console.log(score);
+console.log(time);
+console.log(music);
